435305197592665c5f0f05586a47cc19ea9688c7
Solida2018 <144966179+Solida2018@users.noreply.github.com>
2023-10-02 11:32:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['201206178781', 'ɢᴏᴊᴏ', true],
-  ['967780763155', 'BOT', true], 
+  ['0648323294', 'ɢᴏᴊᴏ', true],
+  ['0648323294', 'BOT', true], 
   [''] 
 ] //Number of owners
 
